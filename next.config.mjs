a365afc8be5a266ec490676e3aac038e28254ba2
wangkexing <1989163733@qq.com>
2024-03-31 14:24:58 +0800

    update yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
+const repo = 'wkx_blog'
+let assetPrefix = `/${repo}/`;
+
+// 用于为应用设置基础路径
+// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
+let basePath = `/${repo}`;
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 const nextConfig = {
-    transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
+  assetPrefix,
+  basePath,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  transpilePackages: [
+    "@douyinfe/semi-ui",
+    "@douyinfe/semi-icons",
+    "@douyinfe/semi-illustrations",
+  ],
 };
 
 export default nextConfig;
