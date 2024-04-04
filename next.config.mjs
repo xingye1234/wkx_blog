2a7a35f34e5b60c23009ed14a158950c2a4ba583
wangkexing <1989163733@qq.com>
2024-04-04 13:09:19 +0800

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const repo = "wkx_blog";
-let assetPrefix = `/${repo}`;
+let assetPrefix = `/${repo}/`;
 
 // 用于为应用设置基础路径
 // 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
@@ -19,6 +19,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    path:'/'
   },
   transpilePackages: [
     "@douyinfe/semi-ui",
