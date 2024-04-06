4c222b7e6c917d4006ea37a075c6cad12ccc6d27
wangkexing <1989163733@qq.com>
2024-04-06 13:53:21 +0800

    solve error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {fetchUsers} from '../lib/action'
 
 export default async function page() {
-  const users = await fetchUsers()
+  // const users = await fetchUsers() 
   return (
     <div>dashboard</div>
   )
