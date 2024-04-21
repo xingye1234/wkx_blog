ab446cb9a2c0f581486a03e34804a8c16fba08fb
wangkexing <1989163733@qq.com>
2024-04-21 13:58:17 +0800

    solve err

--- src/app/dashboard/article/page.tsx
+++ src/app/dashboard/article/page.tsx
@@ -5,14 +5,7 @@ export default async function page() {
  
   return (
     <div className="animate-fade-left animate-duration-1000 animate-delay-100">
-      <div>
-        <LabelCard title="label" link="新增标签">
-        </LabelCard>
-      </div>
-      <div>
-        <LabelCard title="article" link="新增文章">
-        </LabelCard>
-      </div>
+      article
     </div>
   );
 }
