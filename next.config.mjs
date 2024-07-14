1712df7ef8dbc175718008df0ea329b5b9e0d393
wangkexing <1989163733@qq.com>
2024-07-14 20:29:20 +0800

    添加响应式

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
     imgPath: assetPrefix,
   },
   assetPrefix,
-  // basePath, //注：未知原因开发模式下启动找不到页面，需要注释掉
+  basePath, //注：未知原因开发模式下启动找不到页面，需要注释掉
   reactStrictMode: true,
   images: {
     unoptimized: true,
@@ -30,7 +30,8 @@ const nextConfig = {
     "@douyinfe/semi-illustrations",
   ],
   // distDir:"out",
-  output:"standalone",
+  // output:"standalone",
+  output:"export"
 };
 
 export default nextConfig;
